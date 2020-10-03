3dc21bbb2932ff1ae57a165a111d4f4ec77fe91d
Jeff <jeff.neet@gmail.com>
2020-10-03 16:16:20 -0600

    Finished Xylophone scaled roughly with pitch

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:audioplayers/audio_cache.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(XylophoneApp());
@@ -7,10 +8,39 @@ class XylophoneApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              buildKey(flex: 14, color: Colors.red, number: 1),
+              buildKey(flex: 12, color: Colors.orange, number: 2),
+              buildKey(flex: 10, color: Colors.yellow, number: 3),
+              buildKey(flex: 9, color: Colors.green, number: 4),
+              buildKey(flex: 8, color: Colors.teal, number: 5),
+              buildKey(flex: 7, color: Colors.blue, number: 6),
+              buildKey(flex: 6, color: Colors.purple, number: 7),
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  Expanded buildKey({Color color, int number, int flex}) {
+    return Expanded(
+      flex: flex,
+      child: FlatButton(
+        onPressed: () {
+          playSound(number);
+        },
+        color: color,
+      ),
+    );
+  }
+
+  void playSound(int soundNumber) {
+    final player = AudioCache();
+    player.play('note$soundNumber.wav');
   }
 }
